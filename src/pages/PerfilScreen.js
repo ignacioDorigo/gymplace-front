9f880b69b45e6ae7d4b7545bac07b450621e3200
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-06-19 04:30:29 -0300

    button logout

--- src/pages/PerfilScreen.js
+++ src/pages/PerfilScreen.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function PerfilScreen() {
+  const { logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const cerrarSesion = () => {
+    navigate("/home");
+    logout();
+  };
   return (
     <div>
       <Navbar></Navbar>
       <h1>Estoy en perfilScrreen</h1>
+      <button onClick={cerrarSesion}>Cerrar Sesion</button>
       <Footer></Footer>
     </div>
   );
