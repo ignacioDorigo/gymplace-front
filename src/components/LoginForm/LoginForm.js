df58a8d5b2bcbffa7708d63efb8bd47bb716e238
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-06-19 01:26:31 -0300

    version desktop de loginscreen lsita

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,8 +1,66 @@
-import React from 'react'
-import "./LoginForm.css"
+import React from "react";
+import "./LoginForm.css";
+import { useFormik } from "formik";
+import Logo from "../Logo/Logo";
+import { Link } from "react-router-dom";
+import { MdOutlineEmail, MdOutlineLock } from "react-icons/md";
 
 export default function LoginForm() {
+  const estadoInicial = { email: "", password: "" };
+
+  const formik = useFormik({
+    initialValues: estadoInicial,
+    onSubmit: async (formulario) => {
+      console.log(formulario);
+    },
+  });
+
   return (
-    <div>LoginForm</div>
-  )
+    <form className="form" onSubmit={formik.handleSubmit}>
+      <Link to={"/home"}>
+        <img src="/logoNegro.png" className="form__logo" alt="logo" />
+      </Link>
+
+      <h1 className="form__titulo">Bienvenido de vuelta</h1>
+      <p className="form__subtitulo">
+        Inicia sesión para continuar con tu entrenamiento
+      </p>
+
+      <div className="form__campos">
+        <div className="form__campo">
+          <div className="form__input-wrapper">
+            <MdOutlineEmail className="svg__input" />
+            <input
+              id="email"
+              className="form__input"
+              placeholder="Email"
+              type="email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+          </div>
+        </div>
+
+        <div className="form__campo">
+          <div className="form__input-wrapper">
+            <MdOutlineLock className="svg__input" />
+            <input
+              id="password"
+              className="form__input"
+              placeholder="Password"
+              type="password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            />
+          </div>
+        </div>
+      </div>
+
+      <button className="form__button" type="submit">
+        Ingresar
+      </button>
+    </form>
+  );
 }
--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import LoginForm from "../components/LoginForm/LoginForm";
+import "./LoginScreen.css"; // asegurate de crear este archivo
 
 export default function LoginScreen() {
   return (
-    <div>
-      <Navbar></Navbar>
+    <div className="login-screen">
+      <div className="login-screen__imagen" />
+      <div className="login-screen__formulario">
+        <LoginForm />
+      </div>
     </div>
   );
 }
