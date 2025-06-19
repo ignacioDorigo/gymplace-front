f67f52c3e971f02ffbc925cb47e73d8812cc0351
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-06-18 23:07:12 -0300

    agregando Screens

---
+++ src/pages/CalendarioScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CalendarioScreen() {
+  return <div>CalendarioScreen</div>;
+}
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+
+// Pantallas 
+import HomeScreen from '../pages/HomeScreen'
+import PerfilScreen from '../pages/PerfilScreen'
+import ProfesoresScreen from '../pages/ProfesoresScreen'
+import CalendarioScreen from '../pages/CalendarioScreen'
+import ServiciosScreen from '../pages/ServiciosScreen'
 
 export default function AppRouter() {
     return (
-        <div>AppRouter</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='' element={<Navigate to="/home" />} />
+                <Route path='/home' element={<HomeScreen />} />
+                <Route path='/profesores' element={<ProfesoresScreen />} />
+                <Route path='/servicios' element={<ServiciosScreen />} />
+                <Route path='/calendario' element={<CalendarioScreen />} />
+                <Route path='/perfil' element={<PerfilScreen />} />
+                <Route path='/register' element={<RegisterScreen />} />
+                <Route path='/login' element={<LoginScreen />} />
+            </Routes >
+        </BrowserRouter >
     )
 }
